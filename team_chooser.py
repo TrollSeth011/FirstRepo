1512852e3122a7db5f71f569231d0dc796cb9885
Seth Troll <trollseth011@gmail.com>
2017-07-10 09:36:32 -0500

    Programmed team choosero

---
+++ team_chooser.py
@@ -0,0 +1,35 @@
+from random import choice
+
+players = ['Harry', 'Hermione', 'Neville', 'Ginny', 'Luna', 'thisguy']
+teams = ['Alligators', 'Gorrillas', 'Eagles', 'Pythons', 'Wasps', 'Panthers']
+
+team1 = []
+team2 = []
+
+
+def pickteam1():
+    for i in range(len(players)/2):
+        player = choice(players)
+        print(player)
+        team1.append(player)
+        players.remove(player)
+    return team1
+
+def pickteam2():
+    for i in range(len(players)):
+        player = choice(players)
+        print(player)
+        team2.append(player)
+        players.remove(player)
+    return team2
+
+pickteam1()
+pickteam2()
+
+team1name = choice(teams)
+teams.remove(team1name)
+team2name = choice(teams)
+teams.remove(team2name)
+
+print(team1name + ": " + str(team1))
+print(team2name + ": " + str(team2))
