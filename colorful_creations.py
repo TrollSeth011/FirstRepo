1c21665ea5109a77e7a560b34cc1e32ebe75f148
Seth Troll <trollseth011@gmail.com>
2017-07-13 09:10:25 -0500

    Added function to pick the words colors

--- colorful_creations.py
+++ colorful_creations.py
@@ -9,7 +9,7 @@ for line in file:
     c=len(b)-1
     b=b[0:c]
     colours[a]=b
-    
+
 screen  = Screen()
 screen.setup(400,400)
 
@@ -18,9 +18,12 @@ screen.bgcolor(colours['green'])
 
 circle(50)
 dot(300)
-
-color(colours['red'])
-style = ('Arial', 40, 'bold')
-write('Hello', font=style, align='center')
-hideturtle()
+def Colorpicker(colors, word):
+    color(colours[colors])
+    style = ('Arial', 40, 'bold')
+    write(word, font=style, align='center')
+    #hideturtle()
+Colorpicker('orange', 'Hello')
+Colorpicker('red', 'Good')
+Colorpicker('green', 'This')
 screen.exitonclick()
